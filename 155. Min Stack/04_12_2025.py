24cf9620effaf44ca8bd6868d2db20f28c8aef9e
dongYoun2 <dlehddbs6425@gmail.com>
2025-04-12 12:52:17 -0400

    155. Min Stack

---
+++ 155. Min Stack/04_12_2025.py
@@ -0,0 +1,72 @@
+# problem: https://leetcode.com/problems/min-stack/
+# submission: https://leetcode.com/problems/min-stack/submissions/1604755340/
+
+# 23 min
+# TC: all methods (push, pop, top, getMin) are O(1) (required in the problem)
+# SC: O(2*n) -> O(n) (stack + min_idx_stack)
+
+# From LeetCode Top Interview 150 - Stack
+
+# This is the second time solving this problem. The first time I solved this problem is on 2024-04-16.
+
+# At first, I thought of keeping the one minimum index value but quickly realized that it would be problematic when popping the minimum value. So, I came up with the idea of keeping a list of minimum indices. When I was thinking without simulation, I thought there would be a case where the value in the middle of the minimum indices array has to be popped (this requires O(n) time complexity, which violates the problem requirements).
+
+# However, after a simulation, I realized that it is not possible. The only case where the minimum index has to be popped is when the last element in the stack is the minimum value. Therefore, I found out that I could use a stack for the minimum indices as well.
+
+# The LeetCode Editorial section shows that we could also keep the stack for "minimum values" themselves instead of the indices.
+
+# Moreover, it is possible to implement all methods without any if-else conditions. For more details, refer to the markdown file.
+
+
+from collections import deque
+
+
+class MinStack:
+    def __init__(self):
+        self.stack = deque()
+        self.min_idx_stack = deque()
+
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+
+        if len(self.stack) == 1:
+            assert len(self.min_idx_stack) == 0
+
+            self.min_idx_stack.append(0)
+        else:
+            min_idx = self.min_idx_stack[-1]
+
+            if self.stack[min_idx] > val:
+                self.min_idx_stack.append(len(self.stack) - 1)
+
+
+    def pop(self) -> None:
+        assert len(self.stack) != 0
+
+        if self.min_idx_stack[-1] == len(self.stack) - 1:
+            self.min_idx_stack.pop()
+
+        self.stack.pop()
+
+
+    def top(self) -> int:
+        assert len(self.stack) != 0
+        return self.stack[-1]
+
+
+    def getMin(self) -> int:
+        assert len(self.stack) != 0
+        return self.stack[self.min_idx_stack[-1]]
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
+
+# notes while solving the problem:
+# stack: 10 12 4
+# min_indices: 0 2
---
+++ 155. Min Stack/README.md
@@ -0,0 +1,56 @@
+[Problem](https://leetcode.com/problems/min-stack/)
+
+Below is the code that I wrote while solving the above problem on Apr 16, 2024 lol.
+
+Unlike `04_12_2025.py` solution, we could solve the problem without using any if-else conditions. There are two points.
+
+1. Initialize `min_stack` with $2^{31}$, where $2^{31}$ is the one added value from the maximum value that the `val` can take. This is to not consider the (exception) case where `stack` (or `min_stack`) is pushed when it is empty. (This is analogous to using the dummy nodes in a linked list.)
+2. Keep the `min_stack` length the same as `stack` (technically, `len(min_stack) == len(stack) + 1`). Whenever we push or pop the stack, we can also perform the same operation on the `min_stack`.
+
+- [Submission](https://leetcode.com/problems/min-stack/submissions/1234483546/)
+- TC: all methods (push, pop, top, getMin) are $O(1)$ (required in the problem)
+- SC: $O(2*n)$ -> $O(n)$ (stack + min_stack)
+
+```python
+from collections import deque
+
+class MinStack:
+    def __init__(self):
+        self.stack = deque()
+        self.min_stack = deque([2 ** 31]) # init w/ max val to handle inserting val when stack is empty
+
+
+    # this implementation always maintain len(min_stack) == len(stack) + 1
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        self.min_stack.append(min(self.min_stack[-1], val))
+
+
+    def pop(self) -> None:
+        assert bool(self.stack)
+
+        self.stack.pop()
+        self.min_stack.pop()
+
+
+    def top(self) -> int:
+        assert bool(self.stack)
+
+        return self.stack[-1]
+
+
+    def getMin(self) -> int:
+        assert bool(self.stack)
+
+        return self.min_stack[-1]
+
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
+
+```
