feaf2a09f1097702e31bba6716f19ed363dd0cc7
dongYoun2 <dlehddbs6425@gmail.com>
2026-03-07 22:55:42 -0500

    31. Next Permutation

--- 36. Valid Sudoku/03_27_2025.py
+++ 36. Valid Sudoku/03_27_2025.py
@@ -1,11 +1,15 @@
-# problem: https://leetcode.com/problems/valid-sudoku/
 # submission: https://leetcode.com/problems/valid-sudoku/submissions/1588742974/
-
+# runtime: 8 ms (beats 12.07%), memory: 17.69 MB (beats 100%)
 # 29 min
+
 # TC: O(n^2) (Although Sudoku is 9x9, TC is expressed with n for generality, where n is the length of the board. In this situation, each row, column, and sub-box contains the digits 1 to n without duplicates. Also, the size of the sub-box is always (square root of n) x (square root of n), so only the square number can be 'n'.)
 # SC: O(n^2) (python set)
 
-# It was a pretty interesting question. I knew how to transpose a 2D matrix with pure Python. However, it was a bit confusing to map each 3x3 sub-box index to the original index. I could come up with the general index conversion formula by thinking carefully about one example. Each sub-box is represented via (y, x), and each element in the sub-box is represented via (i, j). In fact, this reminds me of the CUDA parallel programming during the MPC course. 1) Mapping the sub-box index to the original index is exactly the same as converting the 2d matrix index to the 1d array index, and 2) mapping the sub-box index to the converted index is exactly the same as computing the global index using the block index and thread index.
+# It was a pretty interesting question. I knew how to transpose a 2D matrix with pure Python. However, it was a bit confusing to map each 3x3 sub-box index to the original index. I could come up with the general index conversion formula by thinking carefully about one example. Each sub-box is represented via (y, x), and each element in the sub-box is represented via (i, j). In fact, this reminds me of the CUDA parallel programming during the MPC course:
+# 1. Mapping to the original board
+# The position (3*y + i, 3*x + j) is like computing a global index from a block index and a thread index (block * size + local). Here (y,x) identifies the sub-box (block) and (i,j) identifies the position inside the box (thread).
+# 2. Mapping to the converted board
+# The position (3*y + x, 3*i + j) flattens both the sub-box index (y,x) and the inner index (i,j) into linear indices. This is similar to converting a 2D index (row, col) into a linear index using row * width + col.
 
 # Below code needs 5 passes (each for transposing matrix, converting sub-boxes to rows, checking rows, columns, and 3x3 sub-boxes) to check the validity of the Sudoku. However, this can be optimized to 1 pass (But the time complexity is still O(n^2)). There are n rows, n columns, and n sub-boxes. For each element in the board, we can check the validity of the row, column, and sub-box at the same time. One thing to notice is how to compute the index for the i-th sub-box. It can be computed by (r//3)*3 + c//3, where r and c are the row and column index of the element in the board. For mor details, refer the LeetCode Editorial.
 
